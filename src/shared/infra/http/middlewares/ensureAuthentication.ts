dd0f5f48b4b7c0ab5cbeeedd65595055edf62812
William <willyeager01@gmail.com>
2022-04-25 19:39:57 -0300

    feat(aws): pre-deploy

--- src/shared/infra/http/middlewares/ensureAuthentication.ts
+++ src/shared/infra/http/middlewares/ensureAuthentication.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
 import authConfig from '@config/auth';
-import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IPayload {
@@ -11,7 +10,6 @@ interface IPayload {
 
 export async function ensureAuthentication(request: Request, response: Response, next: NextFunction): Promise<void> {
   const authHeader = request.headers.authorization;
-  const usersTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError('Token is missing!', 401);
@@ -21,12 +19,6 @@ export async function ensureAuthentication(request: Request, response: Response,
 
   try {
     const { sub: user_id } = verify(token, authConfig.secret_token) as IPayload;
-
-    const user = await usersTokensRepository.findByUserIdAndRefreshToken(user_id, token);
-
-    if (!user) {
-      throw new AppError('User does not exists!', 401);
-    }
 
     request.user = {
       id: user_id,
