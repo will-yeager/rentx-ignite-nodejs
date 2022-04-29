c41fd16c07ff38bf9d7d21c382f558fe977234d8
William <willyeager01@gmail.com>
2022-04-28 21:30:12 -0300

    fix: docker database connection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,8 +5,7 @@ export default async (): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : 'postgres_rentx',
-      database: process.env.NODE_ENV === 'test' ? 'rentx' : 'rentx',
+      database: process.env.NODE_ENV === 'test' ? 'rentx_test' : defaultOptions.database,
     }),
   );
 };
